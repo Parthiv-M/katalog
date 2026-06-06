70568bfd045b59b843bb3d0726ea55d991c92350
parthiv-m <parthivmenon.dev@gmail.com>
2026-06-05 19:32:48 -0700

    Fix display of status bar on smaller screens

--- client/components/ui/StatusBar.tsx
+++ client/components/ui/StatusBar.tsx
@@ -6,7 +6,7 @@ export default async function StatusBar() {
     const lastUpdated = await getLastUpdated();
     const nextScrape = await getNextScrape();
     return (
-        <div className="snap-end shrink-0 w-full bg-black py-2 px-3 text-sm text-neutral-400 flex justify-between">
+        <div className="snap-end shrink-0 w-full bg-black py-2 px-3 text-sm text-neutral-400 flex flex-col gap-1 lg:flex-row lg:gap-0 lg:justify-between">
             <div className="flex items-center">
                 <div className="h-2 w-2 bg-red-200 animate-pulse rounded-full mr-2"></div>
                 <p>Last deployed on {formatToKatalogDate(lastUpdated)}</p>
